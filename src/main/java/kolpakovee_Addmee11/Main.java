147a3861914bf9135788b8d2e24e4627b73f3246
kolpakovee <eekolpakov@edu.hse.ru>
2023-03-21 19:32:31 +0300

    init

--- src/main/java/kolpakovee_Addmee11/Main.java
+++ src/main/java/kolpakovee_Addmee11/Main.java
@@ -8,10 +8,15 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("src/main/resources/type.txt");
+//        File file = new File("src/main/resources/operation_types.txt");
+//
+//        OperationType[] operation_types = objectMapper.readValue(file, OperationType[].class);
+//
+//        for (var el : operation_types){
+//            System.out.printf("id: %s \t name: %s \n", el.id(), el.name());
+//        }
 
-        OperationType op = objectMapper.readValue(file, OperationType.class);
-        System.out.printf("id: %s \t name: %s \n", op.id(), op.name());
+        OperationType[] op = {new OperationType(1, "frying"), new OperationType(17, "pouring boiled water into a paper cup")};
 
         String json = objectMapper.writeValueAsString(op);
 
